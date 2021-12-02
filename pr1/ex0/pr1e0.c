98711ecd464f0265543c705a9ec1aa64ef50f6bc
uzurpastor <sheva0192@gmail.com>
2021-12-02 13:02:47 +0200

    ex0

---
+++ pr1/ex0/pr1e0.c
@@ -0,0 +1,29 @@
+#include "libpr1.h"
+
+
+int main(){
+
+	int n, i, tmp;
+	struct Min x;
+	printf("num: ");
+	scanf("%d", &n);
+
+	int *arr;
+	arr = (int*)calloc(n, sizeof(int));
+
+	set_arr(arr, n);
+	display_arr("input:", arr, n);	
+
+	for(i = 0, x.value = arr[i]; i < n; i++){
+		if(x.value >= arr[i]){
+			x.value = arr[i];
+			x.pos = i;
+		}
+	}
+	remove_item(arr, n, x.pos);
+	n = 1;
+
+	display_arr("output:", arr, n);	
+	free(arr);
+
+}
